506b624e9bb83c802f9ad928bc27514b973709d7
Minuka Dinsitha <it25101443@my.sliit.lk>
2026-05-19 10:51:20 +0530

    Delete backend/realestate-backend directory

--- backend/realestate-backend/src/main/java/com/realestate/backend/repository/PropertyViewingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.realestate.backend.repository;
-
-import com.realestate.backend.model.PropertyViewingRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PropertyViewingRepository extends JpaRepository<PropertyViewingRequest, Long> {
-}
